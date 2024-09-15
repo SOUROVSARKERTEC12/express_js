9dc8aaf03ad34ed76b7a34ad888b4001eb326f93
SOUROV <sourovsarker005@gmail.com>
2024-09-15 19:48:26 +0600

    session object

--- src/server.js
+++ src/server.js
@@ -1,14 +1,27 @@
 import express from "express";
 import routes from "./routes/index"
 import cookieParser from 'cookie-parser';
+import session from "express-session"
 
 const app = express();
 
 app.use(express.json())
 app.use(cookieParser("helloworld"))
+app.use(session({
+    secret: "the dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie: {
+        maxAge: 60000 * 60,
+    }
+}))
 app.use(routes)
 
 app.get('/', (req, res) => {
+    // console.log(req.session);
+    console.log(req.session.id)
+    req.session.visited = true
+
     res.cookie("hello", "world", {maxAge: 30000, signed: true})
     res.status(201).send({message: "Welcome"})
 })
